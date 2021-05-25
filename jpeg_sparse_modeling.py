03ad4a223455c8d3855636fd059a9d1e503ae80b
tr-author <katsushiro@g.ecc.u-tokyo.ac.jp>
2021-05-26 04:45:14 +0900

    refactoring

--- jpeg_sparse_modeling.py
+++ jpeg_sparse_modeling.py
@@ -8,6 +8,7 @@ from cvxpy import *
 import time
 from skimage.metrics import peak_signal_noise_ratio as psnr
 from skimage.metrics import structural_similarity as ssim
+import re
 
 N = 8
 
@@ -63,13 +64,12 @@ class image_JPEG:
             else:
                 compressed_img = np.concatenate([compressed_img,  compressed_img_N_row])
                 qdct_coef = np.concatenate([qdct_coef, qdct_coef_N_row])
-        
+
         compressed_img = correct_abnormal_value(compressed_img.astype(dtype=int))
         qdct_coef = qdct_coef.astype(dtype=int)
         return qdct_coef, compressed_img
 
     def calculate(self,img):
-        print(img.shape)
         dct_coef = self.dct(img)
         
         quantized_dct = self.quantize(dct_coef)
@@ -77,7 +77,6 @@ class image_JPEG:
         self.sparse_num += quantized_dct.size-np.count_nonzero(quantized_dct)
         inv_quantized_dct = self.inv_quantize(quantized_dct)
 
-        #inv_quantized_dct = np.array(np.split(inv_quantized_dct,8))
         invdct_coef = self.idct(inv_quantized_dct)
         
         return quantized_dct,invdct_coef
@@ -96,35 +95,6 @@ class image_JPEG:
         return np.array(np.split(np.round(np.array(dctimg.flatten()) / self.quantizer_table),8))
     def inv_quantize(self,quimg):
         return np.array(np.split(np.array(quimg.flatten()) * self.quantizer_table,8))
-    
-    def compressed_img(self,img,is_quantize=True):
-        image_list = []
-        x,y = img.shape
-
-        for i in range(y//8):
-            for j in range(x//8):
-                im = img[self.N*i:self.N*(i+1),self.N*j:self.N*(j+1)]
-                image_list.append(im)
-
-        for i in range(y//8):
-            for j in range(x//8):
-                if is_quantize==True:
-                    dct_img = np.array(np.split(self.quantize(np.array(np.split(self.dct(image_list[i*(y//N)+j]),8))),8))
-                else:
-                    dct_img = self.dct(image_list[i*(y//N)+j])
-                
-                if j == 0:
-                    pre_coef = dct_img
-                else:
-                    pre_coef = np.concatenate([pre_coef, dct_img], 1)
-            if i == 0:
-                DCT_img = pre_coef
-            else:
-                DCT_img = np.concatenate([DCT_img,  pre_coef])
-            
-        DCT_img = DCT_img.astype(dtype=int)
-        return DCT_img
-
 
 class image_sparse:
     def __init__(self,N):
@@ -142,7 +112,7 @@ class image_sparse:
             return math.sqrt(2/self.N)*math.cos(((2*i+1)*k*math.pi)/(2*self.N))
         
     
-    def compress(self,img):
+    def compress(self,img,lam):
         img_N_division = []
         x,y = img.shape
         if x %8 != 0 or y %8 != 0:
@@ -155,7 +125,7 @@ class image_sparse:
 
         for i in range(y//8):
             for j in range(x//8):
-                dct_coef_N_division,compressed_img_N_division = self.calculate(image_list[i*(y//N)+j])
+                dct_coef_N_division,compressed_img_N_division = self.calculate(img_N_division[i*(y//N)+j],lam)
 
                 if j == 0:
                     compressed_img_row = compressed_img_N_division
@@ -179,18 +149,18 @@ class image_sparse:
         return dct_coef,compressed_img
 
     
-    def calculate(self,img):
-        dct_coef = self.opt_dct_coef(img)
-        self.sparse_num += get_num_nonzero_in_arr(dct_coef)
+    def calculate(self,img,lam):
+        dct_coef = self.opt_dct_coef(img,lam)
+        self.sparse_num += self.get_num_nonzero_in_arr(dct_coef)
         invdct_coef = self.idct(dct_coef)
         
         return dct_coef,invdct_coef
     
-    def get_num_nonzero_in_arr(arr):
+    def get_num_nonzero_in_arr(self,arr):
         arr = arr.flatten()
         return len(arr) - np.count_nonzero(arr)
 
-    def opt_dct_coef(self,img,lam=1.0):
+    def opt_dct_coef(self,img,lam):
         #行列Wを生成
         img -= 128
         W = self.phi
@@ -215,46 +185,13 @@ class image_sparse:
         invdct = np.dot(self.phi, dct_coef.flatten())
         return np.round(np.split(invdct,8)) + 128
     
-    def compressed_img(self,img):
-        image_list = []
-        x,y = img.shape
-
-        for i in range(y//8):
-            for j in range(x//8):
-                im = img[self.N*i:self.N*(i+1),self.N*j:self.N*(j+1)]
-                image_list.append(im)
-
-        for i in range(y//8):
-            for j in range(x//8):
-                dct_img = np.array(np.split(self.dct(image_list[i*(y//N)+j]),8))
-                
-                if j == 0:
-                    pre_coef = dct_img
-                    
-                else:
-                    pre_coef = np.concatenate([pre_coef, dct_img], 1)
-            if i == 0:
-                DCT_img = pre_coef
-            else:
-                DCT_img = np.concatenate([DCT_img,  pre_coef])
-            
-            print("\r","calculateing(incredible inefficient progress), ( 1 ~",y//8,")",i+1,end='')
-
-        print("")   
-        DCT_img = DCT_img.astype(dtype=int)
-        return DCT_img
-
-def calc_entropy(img,is_img):
+def calc_entropy(img):
     l = img.shape[0]
-    
-    if is_img == False:
-        if np.min(img) < 0:
-            img -= np.min(img)
-  
-        histgram = [0]*(np.max(img)+1)
-       
-    else:
-        histgram = [0]*256
+
+    if np.min(img) < 0:
+        img -= np.min(img)
+
+    histgram = [0]*(np.max(img)+1)
 
     for i in range(l):
         for j in range(l):
@@ -281,106 +218,63 @@ def correct_abnormal_value(img):
             if img[i,j] > 255:
                 img[i,j] = 255
 
-def jpeg_img_test():
-    
-    img_test = np.array([
-    [139,144,149,153,155,155,155,155],
-    [144,151,153,156,159,156,156,156],
-    [150,155,160,163,158,156,156,156], 
-    [159,161,162,160,160,159,159,159],
-    [159,161,162,162,162,155,155,155],
-    [161,161,161,161,160,157,157,157],
-    [162,162,161,163,162,157,157,157],
-    [162,162,161,161,163,158,158,158]
-    ])
-
-    plt.imshow(img_test,vmin=0, vmax=255)
-    plt.gray()
-
-    jpeg_test = JPEG(N)
-    #----------------------
-    plt.subplot(1,2,1)
-    plt.imshow(img_test,vmin=0, vmax=255)
-    plt.title("original")
-    converted_img = jpeg_test.convertToJPEG(img_test)
-
-    plt.subplot(1,2,2)
-    plt.imshow(converted_img,vmin=0,vmax=255)
-    plt.title("jpeg converted")
-    plt.gray()
-
-
-    print("\t\tPSNR :",psnr(img_test,converted_img,data_range=255))
-    print("\t\tSSIM :",ssim(img_test,converted_img,data_range=255))
-
-def original_jpeg_convert():
-
-    img = cv2.imread('girl.bmp', 0).astype(dtype=int)
-    img_= cv2.imread('girl.bmp', 0).astype(dtype=int)
-    
-    image = image_JPEG(N)
-
-    plt.figure(figsize=(6, 4))
-    plt.subplot(1,2,1)
-    plt.imshow(img,vmin=0, vmax=255)
-    plt.title("original")
-    plt.gray()
-
-    qdct_coef, compressed_img = image.compress(img_)
-
-    print("\tnumber of dct with zero as its coefficient",image.sparse_num)
-    plt.subplot(1,2,2)
-    plt.imshow(compressed_img,vmin=0,vmax=255)
-    plt.title("jpeg converted")
-
-    print("\t\tentropy :",calc_entropy(qdct_coef,is_img=False))
-
-    print("\t\tPSNR :",psnr(img,compressed_img,data_range=255))
-    print("\t\tSSIM :",ssim(img,compressed_img,data_range=255))
-
-    cv2.imwrite('./jpeg_converted.png', converted_img)
-
-def new_jpeg_convert():
-
-    img = cv2.imread('girl.bmp', 0).astype(dtype=int)
-
-    #----------------------
-    sparse = image_sparse(N)
-
-    print("Compress images in JPEG format with L1 regularization")
-
-    plt.figure(figsize=(6, 4))
-    plt.subplot(1,2,1)
-    plt.imshow(img,vmin=0, vmax=255)
-    plt.title("original")
-    plt.gray()
-
-
-    dct_coef,compressed_img = sparse.compress(img)
-    #compressed = sparse.compressed_img(img_)
-    print("\tnumber of L1 with zero as its coefficient",sparse.sparse_num)
-
-    plt.subplot(1,2,2)
-    plt.imshow(converted_img,vmin=0,vmax=255)
-    plt.title("JPEG format with L1 regularization")
-
-    print("\t\tentropy :",calc_entropy(dct_coef,is_img=False))
-
-    img = cv2.imread('girl.bmp', 0).astype(dtype=int)
-
-    print("\t\tPSNR :",psnr(img,compressed_img,data_range=255))
-    print("\t\tSSIM :",ssim(img,compressed_img,data_range=255))
-
-    cv2.imwrite('./new_jpeg.bmp', converted_img)
-
+    return img
     
 
+def readimg(filename):
+    return cv2.imread(filename, 0).astype(dtype=int)
 if __name__ == "__main__":
     import os
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    #jpeg_img_test()
-    #plt.show()
-    original_jpeg_convert()
-    print("-----------------------------------------------------")
-    new_jpeg_convert()
+
+    filename = "girl.bmp"
+    body = re.split(r'\.',filename)[0]
+    img = readimg(filename)
+    
+    plt.figure(figsize=(6, 4))
+    plt.subplot(1,2,1)
+    plt.imshow(img,vmin=0, vmax=255)
+    plt.title("original")
+    plt.gray()
+
+    image = image_JPEG(N)
+    qdct_coef, compressed_img = image.compress(img)
+    print("\nnumber of dct with zero as its coefficient",image.sparse_num)
+    plt.subplot(1,2,2)
+    plt.imshow(compressed_img,vmin=0,vmax=255)
+    plt.title("jpeg")
+
+    print("\nentropy :",calc_entropy(qdct_coef))
+
+    img = readimg(filename)
+    print("PSNR :",psnr(img,compressed_img,data_range=255))
+    print("SSIM :",ssim(img,compressed_img,data_range=255))
+
+    cv2.imwrite('./'+body+'_jpeg_compress.bmp', compressed_img)
+
+
+    print("\n\nCompress images in JPEG format with L1 regularization")
+
+    lams= [0.01, 0.1 ,1,5]
+    for i,lam in enumerate(lams):
+        print("\n------------------------------------\n")
+        sparse = image_sparse(N)
+        dct_coef,compressed_img = sparse.compress(img,lam)
+        print("lambda=",lam)
+        print("number of L1 with zero as its coefficient",sparse.sparse_num)
+
+        plt.subplot(1,len(lams),i+1)
+        plt.imshow(compressed_img,vmin=0,vmax=255)
+        plt.title("lambda={}".format(lam))
+
+        print("\nentropy :",calc_entropy(dct_coef))
+
+        img = readimg(filename)
+
+        print("PSNR :",psnr(img,compressed_img,data_range=255))
+        print("SSIM :",ssim(img,compressed_img,data_range=255))
+
+        cv2.imwrite('./'+body+'_jpeg_compress_with_lasso(lambda='+str(lam)+').bmp', compressed_img)
+    plt.suptitle("JPEG format with L1 regularization")
     plt.show()
+    
